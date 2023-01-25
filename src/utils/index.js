285e927512f89f446aed3bcb9a8bdf156d597e2f
Julio Rojas <juliorojas81871@gmail.com>
2023-01-24 22:55:28 -0500

    create post frontend done

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 import { surpriseMePrompts } from "../constants";
 
 export function getRandomPrompt() {
-    const radomIndex = Math.floor(Math.random * surpriseMePrompts.length)
+    const radomIndex = Math.floor(Math.random() * surpriseMePrompts.length)
     const randomPrompt = surpriseMePrompts[radomIndex]
 
     if(randomPrompt === prompt) return getRandomPrompt(prompt)
